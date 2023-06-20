9374f53c19b8d39e8799369193d53c72eb8f1852
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-06-20 09:20:33 +0530

    Create 3 - 169. Majority Element.java

---
+++ Arrays Part - III/3 - 169. Majority Element.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int majorityElement(int[] nums) {
+
+        // ***** Moore's Voting Algorithm ***** - 
+
+        int count = 0;
+        
+        int ans = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(count == 0){
+                ans = nums[i];
+            }
+            if(nums[i] != ans){
+                count--;
+            }
+            else{
+                count++;
+            }
+        }
+
+        return ans;
+
+    }
+
+}
