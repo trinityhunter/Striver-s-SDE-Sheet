ba68f99eace32352b3b962161414cd09045ef97e
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-06-22 21:07:03 +0530

    Create 3 - Job Sequencing Problem.java

---
+++ Greedy Algorithm/3 - Job Sequencing Problem.java
@@ -0,0 +1,54 @@
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n){
+        
+        Arrays.sort(arr, (a, b) -> (b.profit - a.profit));
+
+      int maxi = 0;
+      for (int i = 0; i < n; i++) {
+         if (arr[i].deadline > maxi) {
+            maxi = arr[i].deadline;
+         }
+      }
+
+      int result[] = new int[maxi + 1];
+
+      for (int i = 1; i <= maxi; i++) {
+         result[i] = -1;
+      }
+
+      int countJobs = 0, jobProfit = 0;
+
+      for (int i = 0; i < n; i++) {
+
+         for (int j = arr[i].deadline; j > 0; j--) {
+
+            // Free slot found 
+            if (result[j] == -1) {
+               result[j] = i;
+               countJobs++;
+               jobProfit += arr[i].profit;
+               break;
+            }
+         }
+      }
+
+      int ans[] = new int[2];
+      ans[0] = countJobs;
+      ans[1] = jobProfit;
+      return ans;
+        // Your code here
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
